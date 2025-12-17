1f2fd9e976cdfe62ef2d41557d0836af1fee55f2
VladimirLQA <vovaomgxdlazurenko@gmail.com>
2025-12-17 15:29:40 +0200

    feat: binary & linear search

---
+++ search/binarySearch.ts
@@ -0,0 +1,15 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0;
+    let hi = haystack.length;
+    
+    while(lo < hi) {
+        const mid = Math.floor(lo + (hi - lo ) / 2);
+        const currentValue = haystack[mid];
+
+        if (currentValue === needle) return true;
+        else if (currentValue > needle) hi = mid;
+        else lo = mid + 1;
+    }
+
+    return false;
+}
